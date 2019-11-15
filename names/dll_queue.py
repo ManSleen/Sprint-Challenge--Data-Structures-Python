e8e632b3739e4b488074dda1b8dea8c46d14e9c4
ManSleen <mike.vansleen@gmail.com>
2019-11-15 10:28:22 -0700

    Can find duplicate names in 0.127 secs!

---
+++ names/dll_queue.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList(None)
+
+    def __str__(self):
+        return f"size: {self.size}. storage: {self.storage}"
+
+    def enqueue(self, value):
+        self.size += 1
+        return self.storage.add_to_head(value)
+
+    def dequeue(self):
+        if self.storage.tail is not None:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+
+    def len(self):
+        return self.size
--- names/names.py
+++ names/names.py
@@ -1,4 +1,7 @@
 import time
+import sys
+sys.path.append('../binary_search_tree')
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -11,12 +14,16 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+bst1 = BinarySearchTree("Michael VanSleen")
+bst2 = BinarySearchTree("Joseph Rios")
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst1.insert(name_1)
+
+for name_2 in names_2:
+    if bst1.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
