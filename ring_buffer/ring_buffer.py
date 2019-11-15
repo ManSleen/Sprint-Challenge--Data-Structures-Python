04b519e8c6d2c2ff8f0f94143e64195d03a0d1ee
ManSleen <mike.vansleen@gmail.com>
2019-11-15 09:21:29 -0700

    Initial commit, starting ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,21 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def __str__(self):
+        return f"Buffer: {self.storage}"
 
-  def get(self):
-    pass
+    def append(self, item):
+        pass
+
+    def get(self):
+        return [i for i in self.storage if i is not None]
+
+
+ring = RingBuffer(3)
+
+print(ring)
+
+print(ring.get())
